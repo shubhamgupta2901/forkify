17cba4398b82938e7e9c8e01085c67e043b615b5
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-07-29 17:14:04 +0530

    Config changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,6 @@
-const API_KEYS = {
-    'shikhag1409': '62b1badec01d8b7f4f17dd453d58f79d',
-    'shubham.sg2901': 'cd5bfa1c2c8995e9d4c7ac4cc5696e05',
-    'shubham.gupta@quesscorp': '212c8ca43eb357c91ee4c3f0182005ce',
-    'shubham.sg2993': '767de86f8ea74854742098dacb4e2271',
-};
+
 
 export const Config =   {
-    'API_KEY' : API_KEYS['shikhag1409'],
+    'API_KEY' : /*YOUR API KEY HERE */"",
     'BASE_URL' : 'https://www.food2fork.com/api',
 }
